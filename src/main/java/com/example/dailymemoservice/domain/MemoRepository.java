32fd8e05cba56472c831ed24e94c35e562591cbd
Kim-Haul <bokugawa@naver.com>
2023-01-13 17:32:33 +0900

    :memo: Repository 상속 (extends JpaRepository) 추가

--- src/main/java/com/example/dailymemoservice/domain/MemoRepository.java
+++ src/main/java/com/example/dailymemoservice/domain/MemoRepository.java
@@ -1,9 +1,11 @@
 package com.example.dailymemoservice.domain;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface MemoRepository {
+public interface MemoRepository extends JpaRepository<Memo, Long> {
     // JpaRepository 에 미리 작성된 여러코드들(findAll, delete, findById, save)을 갖다 쓸꺼다.
     // List<Memo> findAllByOrderByModifiedAtDesc(); 에서 수정 일자가 24시 전인 것들만 다시 필터링.
 
