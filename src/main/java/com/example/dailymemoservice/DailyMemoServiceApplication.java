6e7eaa08f29e3aea39d75b2824125a28d9d07d6d
Kim-Haul <bokugawa@naver.com>
2023-01-13 16:54:44 +0900

    :memo: Spring Boot 초기 환경설정 및 실행

---
+++ src/main/java/com/example/dailymemoservice/DailyMemoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailymemoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyMemoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyMemoServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dailymemoservice/DailyMemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailymemoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyMemoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
